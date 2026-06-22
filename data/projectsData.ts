cf25a40d5f9bff8de8d19afa38920d577bbf3c9c
Poojesh Shetty <shettypoojesh@live.com>
2026-06-22 21:57:19 +0530

    feat: add centry projects section:

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,17 +3,17 @@ interface Project {
   description: string
   href?: string
   imgSrc?: string
+  link?: string
 }
 
 const projectsData: Project[] = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
+  {
+    title: 'A Logging App like Centry',
+    description: `Centry is my hobby project which is by name has same working as sentry. My main aim was to understand how applications like Sentry works. 
+    So now through centry I got good sense on how logging around frontend and backend works and how to setup them up`,
+    imgSrc: '/static/images/centry/centry-img.png',
+    href: 'https://github.com/PoojeshShetty/centry',
+  },
   // {
   //   title: 'The Time Machine',
   //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
